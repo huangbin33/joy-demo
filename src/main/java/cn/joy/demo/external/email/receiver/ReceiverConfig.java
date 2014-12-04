eb9f74e8c254dc405420521df63a6d0824a87d37
antonio2306 <antonio2306@163.com>
2014-12-04 09:34:23 +0800

    email demo

--- src/main/java/cn/joy/demo/external/email/receiver/ReceiverConfig.java
+++ src/main/java/cn/joy/demo/external/email/receiver/ReceiverConfig.java
@@ -3,11 +3,11 @@ package cn.joy.demo.external.email.receiver;
 public class ReceiverConfig{
 	public final static String mailStorePath = "D:/testmail/";
 	
-	public final static String user = "antonio2306@163.com";
-	//public final static String user = "liyy@actiz.com";
+	public final static String user = "";
+	//public final static String user = "";
 	
-	public final static String pwd = "zhwnshqkdm850217";
-	//public final static String pwd = "actiz1512";
+	public final static String pwd = "";
+	//public final static String pwd = "";
 	
 	public final static String pop3 = "pop3.163.com";
 	//public final static String pop3 = "pop.ym.163.com";
--- src/main/java/cn/joy/demo/external/email/sender/SenderConfig.java
+++ src/main/java/cn/joy/demo/external/email/sender/SenderConfig.java
@@ -3,12 +3,12 @@ package cn.joy.demo.external.email.sender;
 public class SenderConfig{
 	public final static String mailStorePath = "D:/testmail/";
 	
-	public final static String user = "antonio2306@163.com";
+	public final static String user = "";
 	
-	public final static String pwd = "zhwnshqkdm850217";
+	public final static String pwd = "";
 	
 	public final static String smtp = "smtp.163.com";
 	
-	public final static String to = "antonio2306@163.com";
+	public final static String to = "";
 	
 }
